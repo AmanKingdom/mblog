f61aa749aa25a0229a01f33b0ea57c02c4b4700e
AmanKingdom <1424675162@qq.com>
2018-07-21 09:08:48 +0800

    create repository

---
+++ mainsite/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Post
+
+# admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','slug','pub_date')
+
+admin.site.register(Post, PostAdmin)
+
